c92f30ab204e91dee86e61df83df369fb34955a2
javix64 <jav1.x64@gmail.com>
2020-10-05 00:07:45 +0000

    made exercise 4.12

--- t1/exercises/worksheet6.js
+++ t1/exercises/worksheet6.js
@@ -23,3 +23,29 @@ function exercise411() {
         td.append('*');
     }
 }
+
+function exercise412(){
+    var columns=parseInt(prompt("Insert number of columns"))
+    var height =parseInt(prompt("Insert height"))
+    var width =parseInt(prompt("Insert width"))
+    var div411 = document.getElementById('exercise412');
+    var table = document.createElement('table');
+    table.setAttribute('id','tabla');
+    table.setAttribute('border', 1);
+    div411.append(table);
+    var tr = document.createElement('tr');
+    table.append(tr)
+    var td;
+    for (let i = 0; i < columns; i++) {
+        td= document.createElement('td');
+        td.setAttribute('height',height);
+        td.setAttribute('width',width);
+        td.append('*');
+        if (i%2){
+            td.setAttribute("bgcolor","white");
+        }else{
+            td.setAttribute("bgcolor","black");
+        }
+        tr.append(td);
+    }
+}
