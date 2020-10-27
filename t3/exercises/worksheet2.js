6399ce733a292a10e4455b1cbc62c07a5c8283c3
javix64 <jav1.x64@gmail.com>
2020-10-27 12:04:05 +0100

    Added t3/ws2

--- t3/exercises/worksheet2.js
+++ t3/exercises/worksheet2.js
@@ -1,3 +1,32 @@
 function exercise5(){
+    var array1=[];
+    var array2=[];
+    var array3=[];
+    var arrayOrdenado=[];
+    for (let i = 0; i < 100; i++) {
+        var numero=Math.floor(Math.random()*1000+1);
+        array1.push(numero);
+        
+    }
+    console.log("array sin ordenar: "+array1);
+    for (let i = 0; i < array1.length; i++) {
+        if(array1[i]%2==0){
+            array2.push(array1[i]);
+        }else{
+            array3.push(array1[i]);
+        }
+    }
+    arrayOrdenado=array2+array3;
+    console.log("El array ordenado es: "+arrayOrdenado);
+}
+
+function exercise7(){
+    array1= Array(10).fill(1);
+    //array1= Array(10).fill(0).map(x => x++);
+    console.log(array1);
+    console.log(array1.join(" "));
+}
+
+function exercise9(){
     
 }
